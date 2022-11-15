d25188171f1f2a0e13570541a2632b45b4d934b7
DannaMish <zachary_sex30@hotmail.com>
2022-11-14 22:52:04 -0500

    segundo pusheo de proyecto pokemon

---
+++ src/componets/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Card";
+
+const Main=()=>{
+    return(
+        <>
+            <div className="container"></div>
+                <div className="left-content">
+                    <Card/>
+                </div>
+            <div className="right-content">
+            
+            </div>  
+
+        </>
+    )
+}
+
+export default Main;
