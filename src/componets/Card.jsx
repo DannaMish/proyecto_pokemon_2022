02e8e3c8707b6e4ad006ec04f4aeadd0dc23c3b0
DannaMish <zachary_sex30@hotmail.com>
2022-11-15 22:46:23 -0500

    tercer pusheo

--- src/componets/Card.jsx
+++ src/componets/Card.jsx
@@ -5,10 +5,11 @@ const Card=()=>{
         <>
             <div className="Card">
                 <h2>1</h2>
-                <img src="" alt="" />
+                <img src="imagen/charmander.png" alt="" />
                 <h2>Charmander</h2>
             </div>
         </>
+        
     )
 }
 export default Card;
